94bd259a2fbe0f21645c5a07bd104e209d97a7d1
Devon Ryan <dpryan79@gmail.com>
2021-04-02 13:53:19 +0200

    initial commit

---
+++ generateMap.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+library(sf)
+library(ggplot2)
+
+# Load and subset GIS maps
+shps = st_read('GIS/GEM_ew_19.shp')
+
+# Load participant locations
+locs = read.delim("locations.txt")
+
+# Merge GIS and incidence
+shpsi = merge(shps, locs, by.x="GEN", by.y="Location")
+
+# Plot
+annotation = "GIS: © GeoBasis-DE / BKG 2020"
+bgColor="#003974"
+starColor="#d7ab4e"
+g = ggplot(shps) + geom_sf(fill=bgColor, color=bgColor) + theme_classic()
+g = g + theme(axis.line=element_blank(), axis.text=element_blank(), axis.ticks = element_blank(), legend.title=element_text(size=24), legend.text=element_text(size=24), plot.title=element_text(size=24))
+g = g + labs(x="", y="")
+g = g + annotate(geom="text", label=annotation, x=Inf, y=-Inf, hjust=1, vjust=-0.1, size=5)
+g = g + geom_sf_text(data=shpsi, label="★", size=10, color=starColor, family = "HiraKakuPro-W3")
+#g + geom_sf_text(data=st_jitter(shpsi, factor=0.09), label="★", size=5, color="red", family = "HiraKakuPro-W3")
+ggsave("participants.png", plot=g, width=10, height=10)
